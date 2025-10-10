520c756e21f64907301feef8827659aa08273580
Md Rafiqul Islam <rafiverse004@gmail.com>
2025-10-10 20:29:51 +0600

    simple changes for negative values.

--- loops_beginner/13_reverse_number.cpp
+++ loops_beginner/13_reverse_number.cpp
@@ -7,11 +7,22 @@ int main()
     std::cout << "Enter a number: ";
     std::cin >> num;
 
+    bool isNegative = false;
+    if (num < 0)
+    {
+        isNegative = true;
+        num = -num;
+    }
+
     while (num != 0)
     {
         int digit = num % 10;
         reversed = reversed * 10 + digit;
         num = num / 10;
+    }
+    if (isNegative)
+    {
+        reversed = -reversed;
     }
     std::cout << "Reversed: " << reversed;
     return 0;
