0228c61eacbfaaeb04ba76f617a485c998d9ce6c
Md Rafiqul Islam <rafiverse004@gmail.com>
2025-10-10 22:21:40 +0600

    problem 14

---
+++ loops_beginner/14_count_even_odd_digits.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+    int num, digit;
+    int evencount = 0, oddcount = 0;
+
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    if (num < 0)
+    {
+        num = -num;
+    }
+
+    while (num != 0)
+    {
+        digit = num % 10;
+
+        if (digit % 2 == 0)
+        {
+            evencount++;
+        }
+        else
+        {
+            oddcount++;
+        }
+
+        num = num / 10;
+    }
+
+    std::cout << "Even digits: " << evencount << std::endl;
+    std::cout << "Odd digits: " << oddcount << std::endl;
+
+    return 0;
+}
