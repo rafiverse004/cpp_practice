88f004b907bd588af41c7faaddf216686cebfb71
Md Rafiqul Islam <rafiverse004@gmail.com>
2025-10-14 00:57:52 +0600

    Create Bit.cpp

---
+++ codeforces_practice/Bit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::cout << "Enter your choice: " << std::endl;
+    std::cin >> n;
+    
+    int x = 0;
+    std::string statement;
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> statement;
+        if (statement.find("++") != std::string::npos)
+            x++;
+        else
+            x--;
+    }
+
+    std::cout << x;
+    return 0;
+}
