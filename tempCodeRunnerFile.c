e234059190f86b8137e06eb993900d7ff0cf877a
rafiverse004 <rafequlislamcse@gmail.com>
2026-05-08 18:58:17 +0600

    Adds basic C I/O demonstration program
    Introduces a simple C program demonstrating fundamental input and output
    operations. Prompts the user to enter both an integer and a floating-point
    number, then displays the entered values back to the console.

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("%d", number);
+
+    float number1;
+    printf("Enter a number: "); // \n line break use kora hoi mane notun line create kore dei.
+    scanf("%f", &number1);
+    printf("%f", number1);
+}
